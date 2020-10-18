97b62526b617e09d170c150a7aba455abd5fcda2
Alex E <tyrael596@gmail.com>
2020-10-18 19:59:10 +0200

    Wip

---
+++ src/main/java/edu/upc/etsetb/archsoft/spreadsheet/server/chess/entities/ChessPiece.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.upc.etsetb.archsoft.spreadsheet.server.chess.entities;
+
+import java.util.Vector;
+
+
+/**
+ *
+ * @author Alex
+ */
+public abstract class ChessPiece {
+    int ro;
+    int co;
+    boolean color; // White or black 
+    int p;
+    boolean dead; // Status dead or alive   
+    
+    public abstract void isPieceMovement(int ro, int co, int rd, int cd) throws NoPieceMovementException; 
+    abstract public Vector getMovements(int ro, int co, ChessBoard board);
+    
+    
+}
---
+++ src/main/java/edu/upc/etsetb/archsoft/spreadsheet/server/chess/entities/Pawn.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.upc.etsetb.archsoft.spreadsheet.server.chess.entities;
+
+/**
+ *
+ * @author Alex
+ */
+public class Pawn extends ChessPiece{
+ 
+   
+    abstract public ChessPiece transform(ChessPiece type);
+    
+    
+}
